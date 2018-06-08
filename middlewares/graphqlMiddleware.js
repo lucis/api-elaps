75bd26a7fda4de0d82a1a0182e31dc080ac14ed7
Luciano de Oliveira Júnior <luciannojunior@gmail.com>
2018-06-08 00:43:23 -0300

    Finalizando o import dos schemas

--- middlewares/graphqlMiddleware.js
+++ middlewares/graphqlMiddleware.js
@@ -1,3 +1,4 @@
+const { importSchema } = require('graphql-import');
 const resolvers = require('./../resolvers');
 
 const graphqlMiddleware = {};
@@ -5,66 +6,8 @@ const graphqlMiddleware = {};
 const { ApolloServer, gql } = require('apollo-server');
 const { registerServer } = require('apollo-server-express');
 
-const typeDefs = gql`
-  type Cliente {
-      id: String
-      nome: String
-      dataDeNascimento: String
-      endereco: Endereco
-      cpf: String
-      rg: String
-      email: String
-      telefones: Telefones
-      referencia: String
-      avatar: String
-  }
+const typeDefs = gql(importSchema('./graphql/schema.graphql'));
 
-  input ClienteInput {
-    nome: String
-    dataDeNascimento: String
-    cpf: String
-    rg: String
-    email: String
-    referencia: String
-    avatar: String
-    rua: String
-    n: String
-    cep: String
-    cidade: String
-    uf: String
-    bairro: String
-    complemento: String
-    telefonePrincipal: String
-    telefoneAlternativo: String
-  }
-
-  type Telefones {
-      principal: String
-      alternativo: String
-  }
-
-  type Endereco {
-      rua: String
-      n: String
-      cep: String
-      cidade: String
-      uf: String
-      bairro: String
-      complemento: String
-  }
-
-  type Query {
-    cliente(id: String!) : Cliente
-    clientes : [Cliente]
-  }
-
-  type Mutation {
-    addCliente(fields: ClienteInput): Cliente
-  }
-`;
-
-console.log(resolvers);
-// TODO: Conseguir ler os tipos dos arquivos
 const server = new ApolloServer({
   typeDefs,
   resolvers
