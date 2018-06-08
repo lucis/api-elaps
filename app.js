9472785094487d54ab7d6def3de641e4b859826e
Luciano de Oliveira Júnior <luciannojunior@gmail.com>
2018-06-08 00:56:49 -0300

    Adicionando README

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ moment.locale('pt-br');
 
 mongoose.Promise = global.Promise;
 
+// TODO: ler de um arquivo ;)
 global.SECRET = 'GabrielCampos';
 
 const ioMiddleware = require('./middlewares/ioMiddleware');
@@ -18,15 +19,15 @@ routerMiddleware.set(app);
 graphqlMiddleware.set(app);
 
 let url;
-if (process.env.VILAR_MONGO_URL && process.env.VILAR_DATABASE){
-    url = process.env.VILAR_MONGO_URL + process.env.VILAR_DATABASE;
+if (process.env.E_LAPS_MONGO_URL && process.env.E_LAPS_DATABASE){
+    url = process.env.E_LAPS_MONGO_URL + process.env.E_LAPS_DATABASE;
 }else{
     url = 'mongodb://127.0.0.1/elaps'
 }
 
 mongoose.connect(url);
 
-const porta = process.env.API_VILAR_DEMOLAY_PORT || 5002;
+const porta = process.env.API_E_LAPS_DEMOLAY_PORT || 5002;
 app.listen(porta, ()=>{
     console.log('Servidor escutando na porta ' + porta + ' - Data: ' + moment().format('DD/MM/YYYY HH:MM:SS'));
 });
